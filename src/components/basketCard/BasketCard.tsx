f30afc5a5803a590b8ca88d44138ef6e9fc9c3d1
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-17 18:08:56 +0330

    style: add padding to basketcard

--- src/components/basketCard/BasketCard.tsx
+++ src/components/basketCard/BasketCard.tsx
@@ -6,9 +6,9 @@ type BasketCardProps= {
 
 function BasketCard({name,quantity,price}:BasketCardProps) {
   return (
-    <div className="bg-gray-300">
+    <div className="bg-gray-200 p-4 flex flex-col gap-2">
       <p className="text-xl">{name}</p>
-      <div className="bg-blue-400">{quantity} piece for {quantity*price}$</div>
+      <div className="bg-blue-400 w-fit p-1 text-white">{quantity} piece for {quantity*price}$</div>
     </div>
   )
 }
